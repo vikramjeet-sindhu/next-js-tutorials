fda9570f8059f680d01b470c4f026bfb40582296
vikramjeet-sindhu <110517326+vikramjeet-sindhu@users.noreply.github.com>
2023-08-21 17:34:53 +0530

    Update _app.tsx

--- next-misc/pages/_app.tsx
+++ next-misc/pages/_app.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Header from '@/components/layout/Header.js'
-import Footer from @/components/layout/Footer.js'
+import Header from '&/layout/Header'
+import Footer from '&/layout/Footer'
 import '@/styles/layout.css'
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
@@ -10,19 +10,19 @@ import type { NextPage } from 'next'
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
- 
+
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
-  if(Component.getLayout){
+  if (Component.getLayout) {
 
-  return Component.getLayout(<Component {...pageProps} />)
+    return Component.getLayout(<Component {...pageProps} />)
 
   }
 
 
-  return (<><Head><title>nextjs</title><meta name='description' content='nextjs tutorials by vishwas'></meta></Head><Header/><Component {...pageProps} /><Footer/></>)
+  return (<><Head><title>nextjs</title><meta name='description' content='nextjs tutorials by vishwas'></meta></Head><Header /><Component {...pageProps} /><Footer /></>)
 }
